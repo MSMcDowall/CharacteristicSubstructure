3c6b629f1b1095b0c9ac69aa41876d15d41b8bbd
Mary McDowall <mary@Baldrick.lan>
2015-06-21 21:12:45 +0100

    Created Graph class with addVertex method + tests

--- GraphADT.py
+++ GraphADT.py
@@ -1,19 +1,34 @@
 class Vertex:
-	def __init__(self, id, type):
-		self.id = id
-		self.type = type
+	def __init__(self, position, element):
+		self.position = position
+		self.element = element
 		self.adjacent = {}
 
-	def getId(self):
-		return self.id
+	def getPosition(self):
+		return self.position
 
-	def getType(self):
-		return self.type
+	def getElement(self):
+		return self.element
+		
+	def addAdjacent(self, otherPosition, weight):
+		self.adjacent[otherPosition] = weight
 
 	def getAdjacentVertices(self):
 		return self.adjacent.keys()
 
-	def addAdjacent(self, otherId, weight):
-		self.adjacent[otherId] = weight
 		
+class Graph:
+	def __init__(self, name):
+		self.name = name
+		self.vertices = []
+		self.breakpoints = {}
+		self.numVertices = 0
 		
+	def addVertex(self, element):
+		self.vertices.append(Vertex(self.numVertices, element))
+		self.numVertices =+ 1
+	
+	def getVertex(self, index):
+		return self.vertices[index]
+		
+				
--- Tests.py
+++ Tests.py
@@ -1,17 +1,29 @@
-from GraphADT import Vertex
+import GraphADT
 import unittest
 
-class TestGraphADt(unittest.TestCase):
+class TestVertex(unittest.TestCase):
 	
 	def setUp(self):
-		self.x = Vertex(1, 'C')
-		self.x.addAdjacent(2, 'Double')
+		self.x = GraphADT.Vertex(1, 'C')
 		
 	def testVertexCreation(self):
-		self.assertEqual(self.x.getId(), 1)
+		self.assertEqual(self.x.getPosition(), 1)
 
 	def testAddAdjacent(self):
+		self.x.addAdjacent(2, 'Double')
 		self.assertEqual(str(self.x.adjacent.get(2)), 'Double')
+	
+		
+class TestGraph(unittest.TestCase):
+	
+	def setUp(self):
+		self.G = GraphADT.Graph('Bob')
+		
+	def testAddVertex(self):
+		self.G.addVertex('Carbon')
+		self.assertEqual(self.G.getVertex(0).getElement(), 'Carbon')
+		self.G.addVertex('Nitrogen')
+		self.assertEqual(self.G.getVertex(1).getElement(), 'Nitrogen')
 		
 def main():
 	unittest.main()
