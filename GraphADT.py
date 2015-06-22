d2d9a91484082df5e5b75f44dec25ce5dc30a10c
Mary McDowall <mary@Baldrick.lan>
2015-06-22 01:10:29 +0100

    Implemented basic Graph ADT with test

--- GraphADT.py
+++ GraphADT.py
@@ -1,34 +1,70 @@
+# A single Vertex of Graph which includes its chemical element and a dictionary of adjacent vertices
 class Vertex:
 	def __init__(self, position, element):
 		self.position = position
 		self.element = element
 		self.adjacent = {}
 
+	# The postition of the Vertex in its Graph is used as an identifier
 	def getPosition(self):
 		return self.position
 
+	# Return the element coloring of the vertex
 	def getElement(self):
 		return self.element
-		
-	def addAdjacent(self, otherPosition, weight):
+	
+	# Add the indentifying position of an adjacent vertex
+	# and the weight of the bond between them (S, D, or T)
+	def addAdjacent(self, otherPosition, weight='S'):
 		self.adjacent[otherPosition] = weight
 
+	# Return the positions of each of the adjacent vertices in the Graph
 	def getAdjacentVertices(self):
-		return self.adjacent.keys()
+		return self.adjacent
+		
 
-		
+
+# A Graph object which maintains a dictionary of Vertex objects		
 class Graph:
-	def __init__(self, name):
-		self.name = name
-		self.vertices = []
+	def __init__(self):
+		# Dictionary of vertices that have been added to the Graph
+		self.vertices = {}
+		# Stores the elements whose bonds have been broken within a ring
+		# The break signifying number is used as a key
 		self.breakpoints = {}
+		# The number of vertices within the graph
 		self.numVertices = 0
-		
-	def addVertex(self, element):
-		self.vertices.append(Vertex(self.numVertices, element))
+	
+	# Creates a new Vertex object and adds it to the list at the given index postion	
+	def addVertex(self, element, position):
+		newVertex = Vertex(position, element)
+		self.vertices[position] = newVertex
 		self.numVertices =+ 1
 	
-	def getVertex(self, index):
-		return self.vertices[index]
+	# Returns the Vertex object which is at the given position if it exists
+	def getVertex(self, position):
+		if position in self.vertices:
+			return self.vertices[position]
+		else:
+			return None
+	
+	# Deletes the Vertex object at the given position
+	def removeVertex(self, position):
+		if position in self.vertices:
+			del self.vertices[position]
+			
+	# Add a weighted Edge between the Vertices at the two given positions
+	def addEdge(self, firstPosition, secondPosition, weight='S'):
+		if firstPosition in self.vertices and secondPosition in self.vertices:
+			self.vertices[firstPosition].addAdjacent(secondPosition, weight)
+			self.vertices[secondPosition].addAdjacent(firstPosition, weight)
+		else: 
+			print 'Vertex missing'
 		
-				
+	# Remove the edge between the Vertices at the two given positions
+	def removeEdge(self, firstPosition, secondPosition):
+		if firstPosition in self.vertices and secondPosition in self.vertices:
+			del self.vertices[firstPosition].adjacent[secondPosition]
+			del self.vertices[secondPosition].adjacent[firstPosition]
+		else:
+			print 'Vertex missing'
--- Tests.py
+++ Tests.py
@@ -11,19 +11,36 @@ class TestVertex(unittest.TestCase):
 
 	def testAddAdjacent(self):
 		self.x.addAdjacent(2, 'Double')
-		self.assertEqual(str(self.x.adjacent.get(2)), 'Double')
+		self.assertEqual(self.x.adjacent.get(2), 'Double')
 	
 		
 class TestGraph(unittest.TestCase):
 	
 	def setUp(self):
-		self.G = GraphADT.Graph('Bob')
+		self.G = GraphADT.Graph()
+		self.G.addVertex('Carbon', 0)
+		self.G.addVertex('Nitrogen', 1)
+		self.G.addEdge(0, 1, 'D')
 		
 	def testAddVertex(self):
-		self.G.addVertex('Carbon')
 		self.assertEqual(self.G.getVertex(0).getElement(), 'Carbon')
-		self.G.addVertex('Nitrogen')
 		self.assertEqual(self.G.getVertex(1).getElement(), 'Nitrogen')
+	
+	def testRemoveVertex(self):
+		self.G.removeVertex(0)
+		self.assertIsNone(self.G.getVertex(0))
+		
+	def testAddEdge(self):
+		
+		self.assertEqual(self.G.getVertex(0).getAdjacentVertices(), {1: 'D'})
+		self.assertNotEqual(self.G.getVertex(1).getAdjacentVertices(), {})
+		
+	def testRemoveEdge(self):
+		self.G.removeEdge(1, 0)
+		self.assertNotEqual(self.G.getVertex(0).getAdjacentVertices(), {1: 'D'})
+		self.assertEqual(self.G.getVertex(1).getAdjacentVertices(), {})
+				
+	
 		
 def main():
 	unittest.main()
