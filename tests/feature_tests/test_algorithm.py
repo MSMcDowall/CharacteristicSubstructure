49ae0873fbebd0bc935e3727e2ce83cc01ee7f96
Mary McDowall <msmcdowall@gmail.com>
2015-09-01 16:20:35 +0100

    worked on algorithm tests

--- tests/feature_tests/test_algorithm.py
+++ tests/feature_tests/test_algorithm.py
@@ -1,18 +1,22 @@
 from ...source import algorithm
 import unittest
+import os
 
 
 class AlgorithmFeatureTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.tester = algorithm.CSAlgorithm("")
 
-    def test_data_input(self):
-        # What happens when file is empty?
-        # What happens when file isn't smiles?
-        pass
-
-    def test_all_find_representative_structures(self):
+    def test_find_all_representative_structures(self):
         pass
 
     def test_find_characteristic_substructure(self):
+        pass
+
+    def test_argument_input(self):
+        pass
+
+    def test_result_from_empty_file(self):
+        print os.path.relpath("test/test_data/EmptyFile.txt", "source")
+        self.tester.smiles_file = os.path.relpath("test", "source")
         pass
